d777362741a947d14f78eedf41ccbf2cbb642a99
Alex Petersen <theoperatore@gmail.com>
2020-05-17 09:48:38 -0400

    enable graphql endpoint with types

---
+++ src/schema/pantry.graphql.ts
@@ -0,0 +1,37 @@
+import { gql } from 'apollo-server-micro';
+
+export type PantryContext = {
+  // should I put loaders here? or just require them
+  // might just require them
+  // the internet seems to think everything shared should go here...
+  user: string | null;
+};
+
+export const schema = gql`
+  type Query {
+    pantry: [PantryItem!]!
+  }
+
+  type PantryItem {
+    id: ID!
+    name: String!
+    icon_url: String!
+    is_deleted: Boolean!
+    expires_in: String!
+    created_at_ts: String!
+    quantity_type: QuantityType!
+    quantities: [Quantity!]!
+  }
+
+  type Quantity {
+    added_date_ts: String!
+    last_modified_ts: String!
+    is_deleted: Boolean!
+    quantity: Int!
+  }
+
+  enum QuantityType {
+    range
+    unit
+  }
+`;
